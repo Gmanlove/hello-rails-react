89fb2a915da56e094610e104a9024a99ca4ff2f9
Gmanlove <emmagmanc@gmail.com>
2023-07-11 10:50:22 +0100

    Fix linter

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,4 @@
 # app/models/message.rb
 class Message < ApplicationRecord
-    validates :content, presence: true, length: { minimum: 5, maximum: 200 }
-  end
-  
+  validates :content, presence: true, length: { minimum: 5, maximum: 200 }
+end
